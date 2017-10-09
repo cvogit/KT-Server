4318a5e002b0563683ff500f77aeee9ceff97cc4
cvogit <cvogit@gmail.com>
2017-10-09 14:42:01 -0700

    Changing routes.

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,47 @@ $router->put('register', 'RegisterController@register');
 
 $router->get('login', 'AuthController@login');
 
+// JWT protecteed routes
 $router->group(['middleware' => 'jwt'], function () use ($router) {
 
-  $router->get('/test', function ()    {
-		return "JWT checked in.";
-  });
+	// Manager Routes
+  $router->group(['middleware' => 'manager'], function () use ($router) {
 
+  	$router->get('manager', function () {
+		    return "Hello manager.";
+		});
+  	//// Get routes ///
+
+  	// Return all active users
+  	$router->get('/users', 'UserController@getUsers');
+
+  	// Return user with id
+  	$router->get('/users/{id}', 'UserController@getUser');
+
+  	// Return all active teachers
+  	$router->get('/teachers', 'TeacherController@getTeachers');
+
+  	// Return teacher with id
+  	$router->get('/teachers/{id}', 'TeacherController@getTeachers');
+
+  	//// Put routes ////
+
+  	// Activate a user
+	  $router->get('/activate/{id}', 'UserController@activate');
+
+	  // Deactivate a user
+	  $router->get('/deactivate/{id}', 'UserController@deactivate');
+	});
+
+  // Teacher routes
+  $router->group(['middleware' => 'teacher'], function () use ($router) {
+
+  	//// Get routes ////
+  	
+  	// Get all students associate with teacher request
+  	$router->get('/students', 'StudentController@getStudents');
+
+  	// Put routes
+	  $router->get('/students/{id}', 'StudentController@updateStudent');
+	});
 });
