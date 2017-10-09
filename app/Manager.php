4e7ab35f1d92cfcc0124e69d330035d14c8c5b17
cvogit <cvogit@gmail.com>
2017-10-09 14:40:01 -0700

    Create Manager database object.

---
+++ app/Manager.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+
+class Manager extends Model 
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'userId'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+}
