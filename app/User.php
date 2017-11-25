85cb000d08140568bc8a0cd4f17b3def093bef0f
cvogit <cvogit@gmail.com>
2017-11-24 17:59:42 -0800

    Update user model with first name and last name.

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Model
 	 * @var array
 	 */
 	protected $fillable = [
-		'name', 'email', 'password', 'phoneNum'
+		'firstName','lastName', 'email', 'password', 'phoneNum'
 	];
 
 	/**
